70a25f0655555302ef37c3c81b6318edca8a6d61
doobie <sgtmajordoobie@live.com>
2023-03-22 19:47:45 -0400

    [Set up db-service] Initialized project with docker files and basic fastapi endpoint

---
+++ db-service/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Project set up properly"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080,
+                log_level="info")
