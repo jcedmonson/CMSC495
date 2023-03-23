14592b68c94fb58c20c6a9687f61da6eee5eaeed
jedmonson <jedmonson1478@gmail.com>
2023-03-23 00:00:47 -0400

    initial vue, vuetify, pinia

---
+++ front-end/src/components/LoginCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card class="pa-3">
+    <v-card-title>
+      <h3 class="font-weight-light mb-3" v-if="!newUser"> <v-icon icon="mdi-login" class="mb-1"></v-icon> Login</h3>
+      <h3 class="font-weight-light mb-3" v-else> <v-icon icon="mdi-account-plus" class="mb-1"></v-icon> Account Creation</h3>
+    </v-card-title>
+    <v-card-text v-if="newUser">Please enter the required information to join.</v-card-text>
+    <v-form class="pl-2 pr-2" v-if="!newUser">
+      <v-text-field
+        variant="outlined"
+        label="Username"
+        v-model="user.username"
+      ></v-text-field>
+      <v-text-field
+        variant="outlined"
+        type="password"
+        label="Password"
+        v-model="user.password"
+      ></v-text-field>
+      <v-btn block color="primary" @click="user.login">Login</v-btn>
+    </v-form>
+    <v-form class="pl-2 pr-2" v-else>
+      <v-text-field
+        variant="outlined"
+        label="First Name"
+        v-model="user.firstName"
+      ></v-text-field>
+      <v-text-field
+        variant="outlined"
+        label="Last Name"
+        v-model="user.lastName"
+      ></v-text-field>
+      <v-text-field
+        variant="outlined"
+        label="Email"
+        v-model="user.email"
+      ></v-text-field>
+      <v-text-field
+        variant="outlined"
+        label="Username"
+        v-model="user.username"
+      ></v-text-field>
+      <v-text-field
+        variant="outlined"
+        type="password"
+        label="Password"
+        v-model="user.password"
+      ></v-text-field>
+      <v-btn v-if="!newUser" color="primary" @click="user.login">Login</v-btn>
+      <v-btn v-else block color="primary" @click="user.login">Join</v-btn>
+    </v-form>
+    <v-card-actions v-if="!newUser">
+      <v-spacer></v-spacer>
+      <v-btn class="mt-3" variant="text" prepend-icon="mdi-account-plus" @click="newUser = true"
+        >New Account</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { userData } from "@/store/user";
+
+let newUser = ref(false)
+
+const user = userData();
+</script>
+
+<style></style>
