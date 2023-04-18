8d0855192233094d9a8384812b09865f01c67a9a
doobie <sgtmajordoobie@live.com>
2023-04-18 09:22:18 -0400

    Finally fixed the bug with pytest async functions

--- auth_service/tests/test_auth.py
+++ auth_service/tests/test_auth.py
@@ -1,12 +1,28 @@
+import asyncio
+
 import pytest
 from httpx import AsyncClient
 
 from auth_app.main import auth_app
 
-@pytest.fixture
-async def async_app_client():
+@pytest.fixture(scope="session")
+def event_loop():
+    """
+    There is a bug with scope=module, async pytest, and event loops. To fix
+    it you must declare an event loop as "session" and use that accross
+    all the async pytest functions
+    """
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.fixture(scope="module")
+async def async_app_client(event_loop):
     async with AsyncClient(app=auth_app, base_url='http://127.0.0.1:8888') as client:
+        await auth_app.router.startup()
         yield client
+        await auth_app.router.shutdown()
 
 
 async def test_login_user_not_found(async_app_client):
@@ -19,7 +35,7 @@ async def test_login_user_not_found(async_app_client):
     )
     assert response.status_code == 404, response.text
 
-async def test_create_user_valid(async_app_client):
+async def test_create_user_valid(async_app_client: AsyncClient) -> None:
     response = await async_app_client.post(
         "/login",
         json={
@@ -27,5 +43,5 @@ async def test_create_user_valid(async_app_client):
             "password": "password"
         },
     )
-    assert response.status_code == 404, response.text
+    assert response.status_code == 401, response.text
 
