1fc1280825fea275f0756278b9a9513e3399c212
jedmonson <jedmonson1478@gmail.com>
2023-04-19 18:50:54 -0400

    adjustments

--- run_compose.sh
+++ run_compose.sh
@@ -5,7 +5,7 @@ docker volume create --name=data_volume
 docker network inspect cmsc495_network >/dev/null 2>&1 || \
     docker network create --driver bridge cmsc495_network
 
-docker compose up -d
+docker compose -f docker-compose.yml up -d
 
 docker compose logs
 
