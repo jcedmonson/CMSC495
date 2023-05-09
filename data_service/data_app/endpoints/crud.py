77bc401c4764e7910c9ff3d866a47cf457d7acd1
doobie <sgtmajordoobie@live.com>
2023-05-08 23:27:54 -0400

    finished with the posts refactor

--- data_service/data_app/endpoints/crud.py
+++ data_service/data_app/endpoints/crud.py
@@ -9,11 +9,35 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 import dependency_injection as inj
-from models.sql_models import UserProfile, UserConnection, UserPost
+from models.sql_models import UserProfile, UserConnection, UserPost, \
+    PostComment
 from models import padentic_models as p_model
 from endpoints.auth import jwt_token_handler as jwt
 
 log = logging.getLogger("crud")
+
+
+def set_post_model(post: tuple[UserPost, UserProfile]) -> p_model.UserPost:
+    post, user = post
+    return p_model.UserPost(
+        user_id=user.user_id,
+        user_name=user.user_name,
+        first_name=user.first_name,
+        last_name=user.last_name,
+
+        post_id=post.post_id,
+        post_date=post.post_date,
+        content=post.content,
+        comments=post.comments,
+        reactions=post.reactions
+    )
+
+def set_post_models(posts: list[tuple[UserPost, UserProfile]]) -> list[p_model.UserPost]:
+    post_objs = []
+    for post in posts:
+        post_objs.append(set_post_model(post))
+    return post_objs
+
 
 
 async def login_user(session: inj.Session_t,
@@ -67,7 +91,7 @@ async def create_user(session: inj.Session_t,
                                           new_user.pop("password"))
 
     new_user = UserProfile(**new_user, password_hash=password_hash,
-                                      user_creation_date=datetime.now())
+                           user_creation_date=datetime.now())
     session.add(new_user)
     await session.commit()
     await session.refresh(new_user)
@@ -75,9 +99,10 @@ async def create_user(session: inj.Session_t,
     return p_model.UserAuthed(**new_user.__dict__)
 
 
-async def get_user(session: AsyncSession, username: str, exact_match: bool=True):
-
-    stmt = select(UserProfile).filter(UserProfile.user_name.ilike(f"%{username}%"))
+async def get_user(session: AsyncSession, username: str,
+                   exact_match: bool = True):
+    stmt = select(UserProfile).filter(
+        UserProfile.user_name.ilike(f"%{username}%"))
 
     result = await session.execute(stmt)
     result = result.scalars().all()
@@ -120,7 +145,6 @@ async def get_all_users(session: AsyncSession) -> Sequence[
 
 async def get_user_by_id(session: AsyncSession,
                          user_id: int) -> p_model.User:
-
     log.debug(f"Fetching user {user_id}")
 
     stmt = select(UserProfile).where(UserProfile.user_id == user_id)
@@ -138,6 +162,7 @@ async def get_user_by_id(session: AsyncSession,
     log.debug(f"Fetched user {result}")
     return result
 
+
 async def get_connections(session: AsyncSession,
                           user_id: int) -> list[UserProfile]:
     # If successful, the user exists
@@ -147,11 +172,12 @@ async def get_connections(session: AsyncSession,
         raise
 
     # Select all the connection IDs that the user has
-    stmt = select(UserConnection).where(UserConnection.current_user_id == user_id)
+    stmt = select(UserConnection).where(
+        UserConnection.current_user_id == user_id)
     result = await session.execute(stmt)
 
     # get the ID of the users the user has a connection with
-    connections = [i.follows_user_id for i in result.scalars().all() ]
+    connections = [i.follows_user_id for i in result.scalars().all()]
     log.debug(f"User {user_id} has {len(connections)} connections")
 
     # Fetch all the users from the given IDs
@@ -161,39 +187,100 @@ async def get_connections(session: AsyncSession,
 
     return connections
 
-async def get_posts(session: AsyncSession, current_user: p_model.UserAuthed) -> list[p_model.UserPost]:
-    stmt = select(UserProfile).where(UserProfile.user_id == current_user.user_id).options(selectinload(UserProfile.posts))
+
+async def set_comment(session: AsyncSession,
+                      current_user: p_model.UserAuthed,
+                      post_obj: p_model.PostComment,
+                      post_id: int):
+    stmt = select(UserPost).where(UserPost.post_id == post_id)
     result = await session.execute(stmt)
-    user = result.scalar_one_or_none()
+    post: UserPost = result.scalar_one_or_none()
 
-    if user is None or not user.posts:
-        return []
-
-    posts = []
-    user_model = p_model.User.from_orm(user)
-    for post in user.posts:
-        """
-    post_id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    post_date: Mapped[datetime]
-    content: Mapped[str] = mapped_column(String(2048))
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("user_profile.user_id"))
-    user_post: Mapped["UserProfile"] = relationship(back_populates="posts")
-        """
-
-        post = p_model.UserPost(
-            user_id=user_model.user_id,
-            post_id=post.post_id,
-            post_date=post.post_date,
-            user_name=user_model.user_name,
-            first_name=user_model.first_name,
-            last_name=user_model.last_name,
-            content=post.content,
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post ID {post_id} was not found"
         )
-        posts.append(post)
-    return posts
 
-async def set_post(session: AsyncSession, current_user: p_model.UserAuthed, post_body: str):
+    new_post = PostComment(
+        post_id=post.post_id,
+        user_id=current_user.user_id,
+        comment_date=datetime.utcnow(),
+        comment=post.content
+    )
+
+    post.comments.append(new_post)
+    await session.commit()
+
+
+async def get_all_posts(session: AsyncSession, limit: int, offset: int):
+    stmt = (
+        select(UserPost, UserProfile)
+        .join(UserProfile)
+        .options(selectinload(UserPost.comments),
+                 selectinload(UserPost.reactions))
+        .order_by(UserPost.post_date.desc())
+        .offset(offset)
+        .limit(limit)
+    )
+
+    posts = (await session.execute(stmt)).all()
+    return set_post_models(posts)
+
+
+async def get_post(session: AsyncSession, post_id: int):
+    stmt = (
+        select(UserPost, UserProfile)
+        .join(UserProfile)
+        .options(selectinload(UserPost.comments),
+                 selectinload(UserPost.reactions))
+        .where(UserPost.user_id == post_id)
+    )
+
+    post = (await session.execute(stmt)).first()
+    return set_post_model(post)
+
+async def get_posts(session: AsyncSession,
+                    current_user: p_model.UserAuthed) -> list[p_model.UserPost]:
+    stmt = (
+        select(UserPost, UserProfile)
+        .join(UserProfile)
+        .where(UserPost.user_id == current_user.user_id)
+        .options(selectinload(UserPost.comments),
+                 selectinload(UserPost.reactions))
+        .order_by(UserPost.post_date.desc())
+    )
+
+    posts = (await session.execute(stmt)).all()
+    return set_post_models(posts)
+
+    # stmt = select(UserProfile).where(
+    #     UserProfile.user_id == current_user.user_id).options(
+    #     selectinload(UserProfile.posts))
+    # result = await session.execute(stmt)
+    # user = result.scalar_one_or_none()
+    #
+    # if user is None or not user.posts:
+    #     return []
+    #
+    # posts = []
+    # user_model = p_model.User.from_orm(user)
+    # for post in user.posts:
+    #     post = p_model.UserPost(
+    #         user_id=user_model.user_id,
+    #         post_id=post.post_id,
+    #         post_date=post.post_date,
+    #         user_name=user_model.user_name,
+    #         first_name=user_model.first_name,
+    #         last_name=user_model.last_name,
+    #         content=post.content,
+    #     )
+    #     posts.append(post)
+    # return posts
+    #
+    #
+async def set_post(session: AsyncSession, current_user: p_model.UserAuthed,
+                   post_body: str):
     post = UserPost(content=post_body,
                     user_id=current_user.user_id,
                     post_date=datetime.utcnow())
@@ -204,7 +291,6 @@ async def set_post(session: AsyncSession, current_user: p_model.UserAuthed, post
 async def set_connection(session: AsyncSession,
                          current_user: p_model.UserAuthed,
                          user_to_follow: p_model.User) -> None:
-
     # If this is successful, then the user being added is an actual user
     try:
         _ = await get_user_by_id(session, user_to_follow.user_id)
@@ -216,7 +302,8 @@ async def set_connection(session: AsyncSession,
 
     # Add the connection between users
     try:
-        session.add(UserConnection(current_user_id=current_user.user_id, follows_user_id=user_to_follow.user_id))
+        session.add(UserConnection(current_user_id=current_user.user_id,
+                                   follows_user_id=user_to_follow.user_id))
         await session.commit()
     except:
         raise HTTPException(
@@ -228,16 +315,17 @@ async def set_connection(session: AsyncSession,
     # Query to ensure that the connection was added
     result = await get_connections(session, current_user.user_id)
 
-
     if len(prev) == len(result):
-        log.error(f"Failed to add connection {current_user.user_id} -> {user_to_follow.user_id}")
+        log.error(
+            f"Failed to add connection {current_user.user_id} -> {user_to_follow.user_id}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to add connection between users",
             headers={"WWW-Authenticate": "Bearer"}
         )
 
-    log.debug(f"Connection {current_user.user_id} -> {user_to_follow.user_id} made")
+    log.debug(
+        f"Connection {current_user.user_id} -> {user_to_follow.user_id} made")
 
 
 async def authenticate_user(session, settings, username,
